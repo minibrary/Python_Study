3b1ae00aeb675808e959770eccbd2ff1f533c845
minibrary <minibrary@gmail.com>
2016-05-26 23:13:53 +0900

    study

---
+++ Container_Loop/4.py
@@ -0,0 +1,20 @@
+input_id = input("아이디를 입력해주세요.\n")
+# real_josh = "josh"
+# real_k8805 = "k8805"
+members = ['josh', 'k8805', 'jun']
+for member in members:
+    if member == input_id:
+        print('Hello, '+member)
+        import sys
+        sys.exit()
+# if member != input_id:
+print('Get Out!')
+
+
+#
+# if real_josh == in_str:
+#     print("Welcome, josh!")
+# elif real_k8805 == in_str:
+#     print("Welcome, k8805!")
+# else:
+#     print("Get Out!")
