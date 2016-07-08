958d9cd8b80a8c7909968d5ead40a983bc9d2ab7
minibrary <h.cho@lecreuset.japan>
2016-07-08 17:54:57 +0900

    update

--- Class/1.py
+++ Class/1.py
@@ -1,13 +1,14 @@
 class Cal(object):
-    def __init__(v1, v2):
-        
+    def __init__(self, v1, v2):
+        print(v1, v2)
+
 
 c1 = Cal(10,10)
-print(c1.add())
-print(c1.subtract())
+#print(c1.add())
+#print(c1.subtract())
 
-c2 = cal(30,20)
-print(c2.add())
-print(c2.subtract())
+c2 = Cal(30,20)
+#print(c2.add())
+#print(c2.subtract())
 
 
